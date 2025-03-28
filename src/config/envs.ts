c96319b0e5755d758e01a99100e51b8793eb0be9
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-03-28 09:29:13 -0500

    Feat: Google auth envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,11 +4,17 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  JWT_SECRET: string;
 }
 
 const envVarsSchema = joi.object({
   PORT: joi.number().required(),
   DATABASE_URL: joi.string().required(),
+  GOOGLE_CLIENT_ID: joi.string().required(),
+  GOOGLE_CLIENT_SECRET: joi.string().required(),
+  JWT_SECRET: joi.string().required(),
 }).unknown(true);
 
 const { error, value } = envVarsSchema.validate(process.env);
@@ -22,4 +28,7 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   DATABASE_URL: envVars.DATABASE_URL,
+  GOOGLE_CLIENT_ID: envVars.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: envVars.GOOGLE_CLIENT_SECRET,
+  JWT_SECRET: envVars.JWT_SECRET,
 }
