8b77ae11eca556fe0fe36b32a48a7933f4b1a252
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-03-27 22:41:48 -0500

    Feat: Microservice config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      }
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,7 +24,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen( envs.PORT );
-  logger.log(`Application is running on port ${envs.PORT}`);
+  await app.listen();
+  logger.log(`Auth Microservice is running on port ${envs.PORT}`);
 }
 bootstrap();
